aa6c1136880823b47ff0c21d65b020895ae021c4
Claude Hatfield <r.hatfield269@gmail.com>
2019-09-13 20:39:07 -0500

    added questions object

---
+++ assests/game.js
@@ -0,0 +1,42 @@
+var questions = [
+	{
+		question: "In the Lion King, where does Mufasa and his family live?", 
+		choice: ["Pride Rock", "Lion Rock", "Grassland Rock", "Animal Rock"],
+		answers: 0,
+	 },
+	 {
+	 	question: "In Beauty and the Beast, how many eggs does Gaston eat for breakfast?", 
+		choice: ["Two Dozen", "One Dozen", "Five Dozen", "None"],
+		answers: 2,
+		
+	 }, 
+	 {
+	 	question: "What was the name of the dragon (god wanna be) in Mulan?", 
+		choice: ["Mhui", "Mushu", "Mushi", "Mudu" ],
+		answers: 1,
+		
+	}, 
+	{
+		question: "How many daughters does King Triton have in The Little Mermaid?", 
+		choice: ["Four", "One", "Nine", "Seven" ],
+		answers: 3,
+		
+	}, 
+	{
+		question: "What was Wall-E’s job as a robot?", 
+		choice: ["Coffee Maker", "Trash Compactor", "Engineer", "Pilot" ],
+		answers: 1,
+		
+	}, 
+	{
+		question: "A recurring pizza brand shows up in numerous Disney-Pixar film. What was this fictional brand?", 
+		choice: ["Pizza Planet", "Pizza Johns", "Pizza Supreme", "Pizza Pizza" ],
+		answers: 0,
+		
+	}, 
+	{
+		question: "In the 1940 animated musical film Fantasia, Disney cast Mickey Mouse as the sorcerer’s apprentice. Who was originally supposed to play the role?", 
+		choice: ["Goofy", "Donald Duck", "Dopey", "Sleepy" ],
+		answers: 2,
+	
+  }];
