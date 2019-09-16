b87be17b219e4666df9f998312dcd6b7543d568b
Claude Hatfield <r.hatfield269@gmail.com>
2019-09-15 21:26:57 -0500

    reworked html and created function for questions

---
+++ assests/javascript/game.js
@@ -0,0 +1,47 @@
+function resetQuestions() {
+	return {
+			q0: {
+					question: "The most common currency in the sixth world is called?",
+					A: "Dollar",
+					B: "Nuyen",
+					C: "CorpBucks",
+					D: "ShadowCents",
+			},
+			q1: {
+					question: "The entity that gives the runners a job usually goes by what alias?",
+					A: "Mr. Richard",
+					B: "Mr. Doe",
+					C: "Mr. John",
+					D: "Mr. Johnson",
+			},
+			q2: {
+					question: "The event that happened that caused magic to come back to the planet is refered as what?",
+					A: "The Awakening",
+					B: "The Change",
+					C: "The Coming of age",
+					D: "The Happening",
+			},
+			q3: {
+					question: "After the internet was destroyed in the Crash of 2029, what is the world wide web network that is used now called?",
+					A: "Internet 2.0",
+					B: "The Matrix",
+					C: "The United Systems Network",
+					D: "Secured Tranfer Networking",
+			},
+			q4: {
+					question: "The souls connection to the body is also called what?",
+					A: "Catatonics",
+					B: "Minds Power",
+					C: "Spiritual Relay",
+					D: "Essence",
+			},
+			q5: {
+					question: "Instead of social security numbers, we now use what for identification purposes?",
+					A: "Sin",
+					B: "Idfg",
+					C: "Numb",
+					D: "Trigger",
+			},
+			
+	}
+}
